bbf4d24e4106ba777c90ae3d764c0ff6ec80f912
tkadjm <tkeane@chicagobooth.edu>
2015-07-29 16:27:49 -0500

    working 01 part 3

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,14 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+
+    id = params["id"].to_i
+    @photo = Photo.find(id).source
+    @caption = Photo.find(id).caption
+    render("show.html.erb")
+
+  end
+
 
 end
